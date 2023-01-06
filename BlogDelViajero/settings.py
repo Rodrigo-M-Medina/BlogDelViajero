e30d2aa6a5cbb2f1cc9a82024c5422e49a4f691a
Rodrigo-M-Medina <rodrigomiguelmedina77@gmail.com>
2023-01-06 20:53:37 -0300

    probando vercel

--- BlogDelViajero/settings.py
+++ BlogDelViajero/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*-$qgr^fmt1upsm_gp5)ux@^le$!l*qgqwr9qtlnknq(1y1zdj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -125,6 +125,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
